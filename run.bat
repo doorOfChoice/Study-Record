6565f6c6b0cbeb4fc39828d8b1939ea6e8b1c4ea
doorOfChoice <kevinkiller766@gmail.com>
2016-10-13 18:09:57 +0800

    改错

---
+++ run.bat
@@ -0,0 +1,8 @@
+git init
+set /p name="你的文件名"
+set /p branch="上传到的文件夹"
+set /p message="输入你提交的注释"
+git add %name%
+git commit -m "%message%"
+git push git@github.com:doorOfChoice/%branch% master
+pause
